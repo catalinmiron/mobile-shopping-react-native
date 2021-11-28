482b8fa2cf30b29d0cb692091dd179144403eb41
Radu Cazacu <cazacuraduic@gmail.com>
2021-11-28 21:58:12 +0200

    Minor fixes

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -4,7 +4,10 @@ import { View, Text } from "react-native";
 const CartScreen = () => {
   return (
     <View>
-      <Text>Cart</Text>
+      <Text>
+        Here we will integrate Redux or Context or Recoil or MobX to show the
+        orders and remove/update
+      </Text>
     </View>
   );
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+import { AiFillApple } from "react-icons/ai";
 
 import ProductList from "./ProductList";
 import CartScreen from "./CartScreen";
@@ -12,8 +13,26 @@ import CartScreen from "./CartScreen";
 const HomeScreen = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="List" component={ProductList} />
-      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen
+        name="List"
+        component={ProductList}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            <Image source={require("../assets/idea.png")} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            <View>
+              <Image source={require("../assets/idea.png")} />;
+            </View>;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
